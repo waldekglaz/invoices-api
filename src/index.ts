5a01c6ec42bc0db5e02b94df7b132dbd2dedca56
Waldemar Glaz <waldekglaz@gmail.com>
2024-10-01 13:02:36 +0200

    cleared, template ready for reuse

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-import express from 'express';
-import userRoutes from './routes/userRoutes';
-import tweetRoutes from './routes/tweetRoutes';
-import authRoutes from './routes/authRoutes';
-import { authenticateToken } from './middlewares/authMiddleware';
+import express from "express";
 
 const app = express();
 app.use(express.json());
-app.use('/user', authenticateToken, userRoutes);
-app.use('/tweet', authenticateToken, tweetRoutes);
-app.use('/auth', authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello "updated" world!');
+app.get("/", (req, res) => {
+  res.send(
+    "Hello, this is the template for a REST API with TypeScript and Express!"
+  );
 });
 
 app.listen(3000, () => {
-  console.log('Server ready at localhost:3000');
+  console.log("Server ready at localhost:3000");
 });
